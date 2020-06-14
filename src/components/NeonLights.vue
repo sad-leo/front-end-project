416fb7d8672af2c932e19dc341ba77814aba79a3
longly smile <1601242027@qq.com>
2020-06-14 22:01:20 +0800

    霓虹灯按钮特效

---
+++ src/components/NeonLights.vue
@@ -0,0 +1,78 @@
+<template>
+<!-- 霓虹灯特效按钮 -->
+  <div>
+    <a href="#">button</a>
+    <a href="#">button</a>
+    <a href="#">button</a>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+div {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #000;
+}
+a {
+  position: relative;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin: 40px 0;
+  color: #22ebff;
+  font-size: 20px;
+  text-transform: uppercase;
+  transition: 0.5s;
+  overflow: hidden;
+  -webkit-box-reflect: below 1px linear-gradient(transparent, #0002);
+}
+
+a:nth-child(1) {
+  filter: hue-rotate(115deg);
+}
+
+a:nth-child(3) {
+  filter: hue-rotate(270deg);
+}
+
+a:hover {
+  background: #21ebff;
+  color: #111;
+  box-shadow: 0 0 50px #21ebff;
+}
+
+a::before {
+  content: "";
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  height: 10px;
+  width: 10px;
+  border-top: 2px solid #21ebff;
+  border-left: 2px solid #21ebff;
+}
+a:hover::before {
+  width: 100%;
+  height: 100%;
+}
+a::after {
+  content: "";
+  position: absolute;
+  bottom: 0px;
+  right: 0px;
+  height: 10px;
+  width: 10px;
+  border-bottom: 2px solid #21ebff;
+  border-right: 2px solid #21ebff;
+}
+a:hover::after {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import NeonLights from '@/components/NeonLights'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    {
+      path: '/neon',
+      name: 'NeonLights',
+      component: NeonLights
+    },
   ]
 })
